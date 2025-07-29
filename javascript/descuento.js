c3cdf683c70fe28f45b89bf7b4eb5073429bf112
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-07-29 18:12:35 -0300

    Ejercicio de descuento terminado, falta dar detalles (agregar botones-mejorar el diseño)

---
+++ javascript/descuento.js
@@ -0,0 +1,15 @@
+let result = document.getElementById("resultado");
+
+let precio = prompt("ingrese el precio");
+let descuento = prompt("ingrese el descuento");
+
+let total = parseInt(precio*(descuento/100));
+
+// aqui usamos el alert para mostrar mediante la pestaña superior en la pagina
+alert(`El precio es : ${precio} el descuento es de : ${descuento} y el total seria de : ${total}`);
+
+// tambien usamos el innerHTML que se usa para poder escribir los resultados en la pagina "agregamos en el html por asi decirlo"
+result.innerHTML = `
+El precio es: ${precio} <br>
+El descuento es de: ${descuento} <br>
+El total con descuento es de ${total}`;
