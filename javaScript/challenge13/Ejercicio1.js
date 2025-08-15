de1d65e9d85345744f36a02ba95f378bd0b8452c
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-08-15 10:04:51 -0300

    Ejercicios del 1 al 7 de Challenge 13 - Arrays, condicionales y bucles, terminados, tambien el 8 que era (optativo)

--- javaScript/challenge13/Ejercicio1.js
+++ javaScript/challenge13/Ejercicio1.js
@@ -128,4 +128,157 @@ FIN EJERCICIO 5
 */
 
 /*
+Array de animales
+
+Crear un array con 5 animales.
+
+Añadir 2 más (uno al inicio y otro al final).
+
+Eliminar el que está en la tercera posición.
+
+Mostrar la cantidad total de animales.
+
+let animales = ["leon", "tigre", "elefante", "jirafa", "puma"];
+console.log(animales);
+
+animales.unshift("cebra");
+console.log(animales);
+
+animales.push("hipopotamo");
+console.log(animales);
+
+animales.splice(2 , 1);
+console.log(animales);
+
+let total=animales.length;
+console.log(`El total de animales es: ${total}`);
+
+----------------
+FIN EJERCICIO 6
+----------------
 */
+
+/*
+7. Cálculo de IMC
+Diseñar un algoritmo que calcule el IMC con peso (kg) y altura (m). 
+Mostrar el resultado con su categoría:
+
+Menos de 18.5 → Peso bajo
+Entre 18.5 y 24.99 → Peso normal
+25 o más → Sobrepeso
+
+
+let kg = parseFloat(prompt("Ingrese el peso: "));
+let m= parseFloat(prompt("Ingrese la altura: "));
+
+let IMC = kg / (m * m);
+
+let categoria;
+switch (true) { 
+  case (IMC < 18.5):
+    categoria = "Peso bajo";
+    break;
+  case (IMC >= 18.5 && IMC <= 24.99):
+    categoria = "Peso normal";
+    break;
+  case (IMC>=25):
+    categoria = "Sobrepeso";
+  default:
+    console.log("El IMC ingresado no esta en las opciones")
+}
+
+console.log("Tu IMC es: " + IMC.toFixed(2));
+console.log("Categoría: " + categoria);
+
+----------------
+FIN EJERCICIO 7
+----------------
+*/
+
+/*
+8. Signo Zodiacal (Optativo)
+Diseñar un algoritmo que obtenga el mes y día de nacimiento y muestre el signo zodiacal.
+
+
+let signos = [
+  "Capricornio", 
+  // 0
+  "Acuario",     
+  // 1
+  "Piscis",      
+  // 2
+  "Aries",       
+  // 3
+  "Tauro",       
+  // 4
+  "Géminis",     
+  // 5
+  "Cáncer",      
+  // 6
+  "Leo",         
+  // 7
+  "Virgo",      
+   // 8
+  "Libra",       
+  // 9
+  "Escorpio",    
+  // 10
+  "Sagitario"    
+  // 11
+];
+
+
+let inicioSignos = [
+  { mes: 1, dia: 20 },  
+  // Acuario
+  { mes: 2, dia: 19 },  
+  // Piscis
+  { mes: 3, dia: 21 },  
+  // Aries
+  { mes: 4, dia: 20 },  
+  // Tauro
+  { mes: 5, dia: 21 },  
+  // Géminis
+  { mes: 6, dia: 21 },  
+  // Cáncer
+  { mes: 7, dia: 23 },  
+  // Leo
+  { mes: 8, dia: 23 },  
+  // Virgo
+  { mes: 9, dia: 23 },  
+  // Libra
+  { mes: 10, dia: 23 }, 
+  // Escorpio
+  { mes: 11, dia: 22 }, 
+  // Sagitario
+  { mes: 12, dia: 22 }  
+  // Capricornio
+];
+
+
+let dia = parseInt(prompt("Ingrese el dia del (1-31):"));
+let mes = parseInt(prompt("Ingrese el mes (1-12)"));
+
+if(mes<2 || mes>12 || dia<1 || dia>31){
+    alert("La fecha es invalida");
+}
+  else{
+    let signo="signo";
+
+    for(let i=0; i<inicioSignos.length;i++){
+        if(
+            (mes === inicioSignos[i].mes && dia>= inicioSignos[i].dia) ||
+            (mes === inicioSignos[(i+1) % 12].mes && dia< inicioSignos[(i+1)%12].dia)
+        ){
+            signo= signos[(i+1) % 12];
+            break;
+        }
+    }
+    alert("Su signo es: "+ signo  );
+  }
+
+----------------
+FIN EJERCICIO 8
+----------------
+  */
+ 
