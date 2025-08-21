54fa8d4da55c8768040296d178ecc3f151c4dd6b
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-08-21 18:46:55 -0300

    Ejercicio 2 del EjerciciosExtraClase mejorada con do while

--- javaScript/EjerciciosExtraClase/Ejercicio2.js
+++ javaScript/EjerciciosExtraClase/Ejercicio2.js
@@ -1,21 +1,41 @@
 // Crea una funcion que reciba un array de numeros y devuelva el mayor de ellos
 
-let numerosArray = [5, 7, 8, 9, 10, 15];
-let mayor=0;
+let numerosArray = [];
+let continuar = true;
 
-function obtenerMayor() {
-    for (let i = 0; i < numerosArray.length; i++) {
-        if (isNaN(numerosArray[i])) {
-            return "Ingrese un valor valido";
-        }
-        else {
-            if (numerosArray[i] > mayor) {
-                mayor = numerosArray[i];
-            }
+do {
+    let input = prompt("Ingrese un numero:");
+    
+    // Si el usuario hace clic en "Cancelar"
+    if (input === null) {
+        continuar = false;
+        break;
+    }
+    
+    let numero = parseInt(input);
+    
+    if (isNaN(numero)) {
+        alert("Ingrese un valor válido");
+    } else {
+        numerosArray.push(numero);
+        continuar = confirm("¿Desea ingresar otro número?");
+    }
+    
+} while (continuar);
+
+function obtenerMayor(array) {
+    if (array.length === 0) {
+        return "No se ingresaron números válidos";
+    }
+    
+    let mayor = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > mayor) {
+            mayor = array[i];
         }
     }
     return mayor;
 }
-let respuesta=obtenerMayor(numerosArray);
-console.log(respuesta);
 
+let respuesta = obtenerMayor(numerosArray);
+console.log(respuesta);
