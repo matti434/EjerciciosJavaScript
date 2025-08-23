42d45cc540e543fb252a6be9736e5bf3bc34adeb
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-08-23 05:40:27 -0300

    Ejercicio 3 y 4 de EjerciciosExtraPoo terminados

--- javaScript/EjerciciosExtraPOO/Ej3Herencia.js
+++ javaScript/EjerciciosExtraPOO/Ej3Herencia.js
@@ -5,9 +5,6 @@ Clase hija Perro con método hacerSonido() que devuelva "Guau!".
 
 Clase hija Gato que devuelva "Miau!".
 
-Clase Empleado con nombre y salario.
-
-Clase hija Gerente con propiedad departamento y método detalles().
 */
 
 class Animal {
@@ -26,36 +23,68 @@ class Perro extends Animal {
   }
 }
 
-class Gato extends Animal{
-    hacerSonido(){
-        return "Miau!";
-    }
+class Gato extends Animal {
+  hacerSonido() {
+    return "Miau!";
+  }
 }
 
 let tipo;
-do{
-    
-    tipo= prompt("Ingrese el tipo de animal (perro/gato)  y (salir) para terminar:").toLowerCase();
-    const nombre = prompt("Ingrese el nombre:");
+do {
+  tipo = prompt(
+    "Ingrese el tipo de animal (perro/gato)  y (salir) para terminar:"
+  ).toLowerCase();
+  const nombre = prompt("Ingrese el nombre:");
 
-    switch(tipo){
-        case "perro":
-            const perro=new Perro(nombre);
-            console.log(`${perro.nombreAnimal} y dice : ${perro.hacerSonido()}`);
-            break;
-        case "gato":
-            const gato=new Gato(nombre);
-            console.log(`${gato.nombreAnimal} y dice : ${gato.hacerSonido()}`);
-            gato.hacerSonido;
-            break;
-        case "salir":
-            console.log("saliendo del programa");
-            break;
+  switch (tipo) {
+    case "perro":
+      const perro = new Perro(nombre);
+      console.log(`${perro.nombreAnimal} y dice : ${perro.hacerSonido()}`);
+      break;
+    case "gato":
+      const gato = new Gato(nombre);
+      console.log(`${gato.nombreAnimal} y dice : ${gato.hacerSonido()}`);
+      gato.hacerSonido;
+      break;
+    case "salir":
+      console.log("saliendo del programa");
+      break;
     default:
-        const animal=new Animal(nombre);
-        console.log(`${animal.nombreAnimal} dice: ${animal.hacerSonido} desconocido...`);
-        break;
-        
-    }
-}while(tipo!=="salir");
+      const animal = new Animal(nombre);
+      console.log(
+        `${animal.nombreAnimal} dice: ${animal.hacerSonido} desconocido...`
+      );
+      break;
+  }
+} while (tipo !== "salir");
 
+/*
+Clase Empleado con nombre y salario.
+
+Clase hija Gerente con propiedad departamento y método detalles().
+*/
+
+class Empleado {
+  constructor(nombre, salario) {
+    this.nombre = nombre;
+    this.salario = salario;
+  }
+}
+
+class Gerente extends Empleado {
+  constructor(departamento) {
+    super(nombre, salario); // para nombre y salario al constructor padre
+    this.departamento = departamento;
+  }
+
+  detalles() {
+    return `Nombre: ${this.nombre}, Salario: ${this.salario}, Departamento: ${this.departamento}`;
+  }
+}
+
+const nombreEmpleado=prompt("Ingrese el nombre del empleado");
+const salario=parseFloat(prompt("Ingrese el salario del empleado"));
+const departamento=prompt("Ingrese a que departamento pertenece");
+
+const gerente=new Gerente(nombreEmpleado,salario,departamento);
+console.log(gerente.detalles);
--- javaScript/EjerciciosExtraPOO/Ej4PooAvanzado.js
+++ javaScript/EjerciciosExtraPOO/Ej4PooAvanzado.js
@@ -26,3 +26,43 @@ class Producto{
       this.precio=precio;
     }
 }
+
+class Carrito{
+
+  constructor(productos){
+    this.productos=[];
+  }
+
+  agregarProductos(producto){
+    this.productos.push(producto)
+     console.log(`${producto.nombre} agregado al carrito ($${producto.precio})`);
+  }
+
+  calcularTotal(){
+    let total=0;
+    for(let producto of this.productos){
+      total+=producto.precio;
+    }
+    return total;
+  }
+
+    mostrarCarrito() {
+    console.log("Carrito de compras:");
+    this.productos.forEach((p, i) => {
+      console.log(`${i + 1}. ${p.nombre} - $${p.precio}`);
+    });
+    console.log(`Total: $${this.calcularTotal()}`);
+  }
+}
+
+const carrito = new Carrito();
+
+const p1 = new Producto("Camiseta", 3000);
+const p2 = new Producto("Pantalón", 5000);
+const p3 = new Producto("Zapatillas", 10000);
+
+carrito.agregarProductos(p1);
+carrito.agregarProductos(p2);
+carrito.agregarProductos(p3);
+
+carrito.mostrarCarrito();
