3b46a35c7abd731abcc23a0b0dd15c4050469da9
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-08-25 06:22:21 -0300

    Comente y cambie una parte del codigo para probar node.js

--- javaScript/EjerciciosExtraPOO/Ej1ClaseObjetos.js
+++ javaScript/EjerciciosExtraPOO/Ej1ClaseObjetos.js
@@ -6,14 +6,13 @@ Instancia 2 objetos y haz que cada uno se presente.
 Clase Coche con propiedades: marca, modelo, año.
 
 Agrega un método detalles() que devuelva "Coche: [marca] [modelo] ([año])".
-*/
 
- class Persona{
+class Persona{
    
-    constructor(nombre,edad){
-        this.nombre=nombre;
-        this.edad=edad;
-    }
+constructor(nombre,edad){
+    this.nombre=nombre;
+    this.edad=edad;
+}
 
     saludar(){
         console.log(` Hola mi nombre es : ${this.nombre} y mi edad es ${this.edad}`);
@@ -32,7 +31,7 @@ Agrega un método detalles() que devuelva "Coche: [marca] [modelo] ([año])".
        this.modelo=modelo;
        this.anio=anio;    
     }
-
+    
     detalles(){
         console.log(`La marca del coche es ${this.marca} su modelo es ${this.modelo} y es del año ${this.anio}`);
     }
@@ -44,3 +43,55 @@ Agrega un método detalles() que devuelva "Coche: [marca] [modelo] ([año])".
  
  const coche=new Coche(marca,modelo,anio);
  coche.detalles();
+ 
+ Dejo comentada por que quiero probar Node pero parece que tiene problemas con los prompt
+ 
+ */
+
+ const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+class Persona {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+
+  saludar() {
+    console.log(`Hola mi nombre es: ${this.nombre} y mi edad es ${this.edad}`);
+  }
+}
+
+class Coche {
+  constructor(marca, modelo, anio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+  }
+
+  detalles() {
+    console.log(`La marca del coche es ${this.marca}, su modelo es ${this.modelo} y es del año ${this.anio}`);
+  }
+}
+
+// Pedimos datos
+rl.question("Ingrese su nombre: ", (nombre) => {
+  rl.question("Ingrese su edad: ", (edad) => {
+    const persona = new Persona(nombre, edad);
+    persona.saludar();
+
+    rl.question("Ingrese la marca del vehículo: ", (marca) => {
+      rl.question("Ingrese el modelo del vehículo: ", (modelo) => {
+        rl.question("Ingrese el año del vehículo: ", (anio) => {
+          const coche = new Coche(marca, modelo, anio);
+          coche.detalles();
+          rl.close();
+        });
+      });
+    });
+  });
+});
