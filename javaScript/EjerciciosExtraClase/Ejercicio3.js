9a2d9e18c45c56e5528fc18a31bb032af7f230d1
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-08-28 22:33:47 -0300

    Mejorado las condicionales de los ejercicios ExtraClase

--- javaScript/EjerciciosExtraClase/Ejercicio3.js
+++ javaScript/EjerciciosExtraClase/Ejercicio3.js
@@ -1,11 +1,16 @@
 // crea una funcion que reciba un string y devuelva el numero de vocales que contiene
 
-let texto = prompt("Ingrese el texto:");
+let texto = prompt("Ingrese el texto:").toLowerCase();
 let contador=0;
 
 function devolucionVocales(texto) {
     if (isNaN(texto)) {
         alert("ingrese valor valido:");
+        return;
+    }
+    if (texto === null || texto === undefined || texto.trim() === ""){
+        alert("Ingrese un texto válido:");
+        return;
     }
     else {
         for (let i = 0; i <= texto.length; i++) {
