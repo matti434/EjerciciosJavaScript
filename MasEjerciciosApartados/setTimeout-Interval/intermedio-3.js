c4e12ba8d592a8cc5f37f3bcc7dbfa64b093c542
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-02 00:43:55 -0300

    Ejercicio 3 intermedio de setTimeout-Interval terminado con distintos tipos

--- MasEjerciciosApartados/setTimeout-Interval/intermedio-3.js
+++ MasEjerciciosApartados/setTimeout-Interval/intermedio-3.js
@@ -12,19 +12,29 @@ new Date(): Crea un nuevo objeto Date con la fecha y hora actual
 
 .toLocaleTimeString(): Convierte la parte de la hora a una cadena 
 formateada según la configuración regional del navegador
-*/
 
 // con formato personalizado
 setInterval(() => {
-    //Crea un nuevo objeto date con la fecha y hora en const hora
-    const ahora = new Date();
-    const hora = ahora.toLocaleTimeString(`es-Es`,{
-        hour: `2-digit`,
-        minute: `2-digit`,
-        second: `2-digit`,
-        hour12: false,
-    });
-    console.log(hora);
+  //Crea un nuevo objeto date con la fecha y hora en const hora
+  const ahora = new Date();
+  const hora = ahora.toLocaleTimeString(`es-Es`,{
+    hour: `2-digit`,
+    minute: `2-digit`,
+    second: `2-digit`,
+    hour12: false,
+  });
+  console.log(hora);
 },1000);
 
+*/
 // Version mas detallada con limpieza
+
+let intervalo;
+function iniciarReloj(){
+  invervalo = setInterval(()=>{
+    console.clear(); // Limpiar consola
+    console.log('Hora actual:', new Date().toLocaleTimeString());
+  },1000);
+}
+
+iniciarReloj();
