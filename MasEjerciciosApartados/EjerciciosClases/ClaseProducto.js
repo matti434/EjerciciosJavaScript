65a2cadd9fc3ba777259017ce8899293a475bb92
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-05 23:06:41 -0300

    Ejercicio ClaseProducto sin terminar, falta validaciones y metodo reponer

---
+++ MasEjerciciosApartados/EjerciciosClases/ClaseProducto.js
@@ -0,0 +1,43 @@
+/*
+Clase Producto
+
+Propiedades: nombre, precio, stock.
+
+Métodos:
+
+vender(cantidad) → descuenta del stock si hay suficiente.
+
+reponer(cantidad) → suma al stock.
+
+mostrarInfo() → devuelve "Producto: [nombre], Precio: $[precio], Stock: [stock]".
+*/
+
+class Producto {
+  constructor(nombre, precio, stock) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+  }
+
+  vender(cantidad) {
+
+    if (cantidad == null) {
+      console.log("Cantidad no proporcionada");
+      return this.stock;
+    }
+    // validar que sea numeri
+    if (typeof cantidad !== "number" || cantidad <= 0) {
+      console.log("❌ La cantidad debe ser un número mayor a 0");
+      return this.stock; // Devolver el stock actual
+    }
+    if (cantidad > this.stock) {
+      console.warn(` Stock insuficiente. Disponible: ${this.stock}, Intentaste vender: ${cantidad}`);
+      return this.stock;
+    } 
+    else {
+      this.stock -= cantidad;
+       console.log(`Vendido: ${cantidad} unidades. Stock restante: ${this.stock}`);
+      return this.stock;
+    }
+  }
+}
