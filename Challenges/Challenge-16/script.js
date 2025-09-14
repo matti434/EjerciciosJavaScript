cc850c9784203818ef6d91535a752e3a3fce1ab8
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-14 12:39:40 -0300

    Challenge terminado agregando funciones nuevas

---
+++ Challenges/Challenge-16/script.js
@@ -0,0 +1,31 @@
+const contador = document.getElementById("counter");
+const btnReiniciar = document.getElementById("reiniciar");
+const btnIncrementar = document.getElementById("incrementar");
+const btnDisminuir = document.getElementById("disminuir");
+const mensaje = document.getElementById("mensaje")
+
+let valor = 0;
+
+btnIncrementar.addEventListener('click',()=>{
+   valor++;
+   contador.textContent=valor;
+   mensaje.textContent="";
+});
+
+btnDisminuir.addEventListener('click',() =>{
+    if(valor>0){
+        valor--;
+        contador.textContent=valor;
+        mensaje.textContent="";
+    }
+    else{
+      mensaje.textContent ="No se puede disminuir llegado al 0"; 
+    }
+})
+
+btnReiniciar.addEventListener('click',()=>{
+    valor=0;
+    contador.textContent=valor;
+    mensaje.textContent="";
+});
+
