9b83cd75a4e39b5d9238faea1ede22eb1873641b
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-16 18:10:07 -0300

    Enunciado español y ingles ejercicio Fizz Buzz

---
+++ MasEjerciciosApartados/EjerciciosLogica/fizz-Buzz.js
@@ -0,0 +1,16 @@
+/*
+Escribe un programa que muestre por consola los numneros de 1 a 100 (con un salto de linea entra cada impresion)
+sustituyendo los siguientes:
+
+Multiplos de 3 por la palabra "fizz".
+Multiplos de 5 por la palabra "buzz".
+Multiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+
+write a program that/esa display/mostrar the numbers from 1 to 100 on the console (with a linea break between each)
+substituting/substituting the following
+
+Multiples of 3 for the word "fizz".
+Multiples of 5 for the word "buzz".
+Multiples of both 3 and 5 for the word "fizzbuzz".
+ */
+
