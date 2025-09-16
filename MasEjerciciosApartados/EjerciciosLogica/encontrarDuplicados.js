926abf0788f8c79fe50e9c0b0c359b0d3e23090a
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-16 20:36:16 -0300

    Ejercicio encontrarDuplicado de ejercicio logica terminado falta version mejorada

---
+++ MasEjerciciosApartados/EjerciciosLogica/encontrarDuplicados.js
@@ -0,0 +1,29 @@
+const arrays = [
+    2,4,6,3,6,9,3,12,87,345,21,36,7,344,7,313,5,2312,2,5,9
+]
+const duplicados=[];
+const visto = {};
+/*
+for(let i=0; i<arrays.length;i++){
+    
+   //si fuera j=0 se compraria siempre el mismno con i
+   for(let j=i+1;j<arrays.length;j++){
+    
+    if(arrays[i] === arrays[j]){
+        duplicados.push(arrays[i]);
+        break;
+    }
+   }
+
+}
+
+console.log("Los numeros son: "+ arrays);
+console.log("Los numeros duplicados son: "+ duplicados);
+*/
+
+function encontrarDuplicados(arrays){
+  //ingresa el array
+  //compraro si fue visto
+
+  visto=arrays;
+}
