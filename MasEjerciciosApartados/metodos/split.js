d1327e483d8188e7e6d3c6ffc95265c85523f5da
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-18 19:43:29 -0300

    Ejericio split.js de metodos terminado

---
+++ MasEjerciciosApartados/metodos/split.js
@@ -0,0 +1,16 @@
+/*
+Convierte un string en un array
+
+Cada carácter se convierte en un elemento del array
+
+El string vacío '' como separador divide por cada carácter
+ */
+const frase = "JavaScript es increíble";
+const resultado = frase.split('');
+//const resultado = frase.split(' '); ["J", "v", "Script es increíble"]
+/*console.log(resultado);
+const dosPrimeras = frase.split(' ', 2);
+console.log(dosPrimeras); // ["JavaScript", "es"]
+const invertido = frase.split('').reverse().join('');
+console.log(invertido); // "elbírcne se tpircSavaJ"
+*/ 
