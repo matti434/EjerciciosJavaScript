82712c5e209132d47a2e39598255074960bb5d4c
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-09-19 19:33:48 -0300

    funcion creada de lista de tareas

--- MasEjerciciosApartados/DOM-Eventos/ListaDeTarea/ListaDeTareas.js
+++ MasEjerciciosApartados/DOM-Eventos/ListaDeTarea/ListaDeTareas.js
@@ -1,3 +1,19 @@
 //To-Do List (lista de tareas) → Agregar, marcar como hechas, eliminar tareas.
 
+// recibir la entrada de las tareas
+// hacer el contenedor de la tarea
+// hacer la lista de las tareas
+// ingresar el contenido de la tarea
+
+
+const crearTarea = (e) => {
+    e.preventDefault();
+    console.log("desde crear tarea");
+    
+    const inputTarea = document.getElementById("taskInput").value.trim();
+    console.log(inputTarea);
+    
+}
+
+formTareas.addEventListener('submit', crearTarea);
 
