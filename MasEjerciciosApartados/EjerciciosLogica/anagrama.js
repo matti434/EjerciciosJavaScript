1ac450b5d34d5b95fd88cf0775bc20b66a6c2e37
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-23 23:35:41 -0300

    Ejercicio anagrama no terminado, hay que corregir. De Ejercicios logica

--- MasEjerciciosApartados/EjerciciosLogica/anagrama.js
+++ MasEjerciciosApartados/EjerciciosLogica/anagrama.js
@@ -10,23 +10,38 @@
  */
 
 function anagrama(string1, string2) {
-  const letra = [];
-  const letra2 = [];
-
-  if (string1.length.toLowerCase() !== string2.length.toLowerCase()) {
+  const contador = {};
+  if (string1.length !== string2.length) {
     console.log(
       "La cantidad de letras es distinta entre las palabra. Por lo que no es palindromo"
     );
     return;
   }
   for (let i = 0; i < string1.length; i++) {
+    const letra = string1[i];
+
     // tengo que guardar cada letra
-    letra = string1[i].toLowerCase();
-    for (let j = 0; j < string1.length; j++) {
-      letra2 = string1[j].toLowerCase();
+    if (contador[letra] === undefined) {
+      contador[letra] = 1;
+    } else {
+      contador[letra] = +1;
     }
-    
+    for (let j = 0; j < string1.length; j++) {
+      const letra = string1[j];
+      if (contador[letra] === undefined) {
+        contador[letra] = 1;
+      } else {
+        contador[letra] = +1;
+      }
+    }
   }
+
+  for (let letra in contador) {
+    if (conteo1[letra] !== conteo2[letra]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 const string1 = "amor";
