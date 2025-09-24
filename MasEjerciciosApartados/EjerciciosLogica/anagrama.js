37114bd774319e39f39df1a3dfb78599eb3af509
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-09-24 12:11:10 -0300

    Ejercicio anagrama solucionado y terminado de Ejercicios logica

--- MasEjerciciosApartados/EjerciciosLogica/anagrama.js
+++ MasEjerciciosApartados/EjerciciosLogica/anagrama.js
@@ -9,40 +9,48 @@
  * - Dos palabras exactamente iguales no son anagrama.
  */
 
-function anagrama(string1, string2) {
+function contarLetras(palabra) {
   const contador = {};
-  if (string1.length !== string2.length) {
-    console.log(
-      "La cantidad de letras es distinta entre las palabra. Por lo que no es palindromo"
-    );
-    return;
-  }
-  for (let i = 0; i < string1.length; i++) {
-    const letra = string1[i];
-
+  for (let i = 0; i < palabra.length; i++) {
+    const letra = palabra[i].toLowerCase();
     // tengo que guardar cada letra
     if (contador[letra] === undefined) {
       contador[letra] = 1;
     } else {
-      contador[letra] = +1;
-    }
-    for (let j = 0; j < string1.length; j++) {
-      const letra = string1[j];
-      if (contador[letra] === undefined) {
-        contador[letra] = 1;
-      } else {
-        contador[letra] = +1;
-      }
+      contador[letra] += 1;
     }
   }
+  return contador
+  /*
+  for (let j = 0; j < string2.length; j++) {
+    const letra = string2[j].toLowerCase();
+    if (contador2[letra] === undefined) {
+      contador2[letra] = 1;
+    } else {
+      contador2[letra] += 1;
+    }
+    if(j === string2.length){
+      return contador2
+    }
+  }*/
+}
+function anagrama(string1, string2) {
+  if (string1.length !== string2.length) {
+    console.log(
+      "La cantidad de letras es distinta entre las palabra. Por lo que no es palindromo"
+    );
+    return false;
+  }
 
-  for (let letra in contador) {
+  const conteo1 = contarLetras(string1);
+  const conteo2 = contarLetras(string2);
+
+  for (let letra in conteo1) {
     if (conteo1[letra] !== conteo2[letra]) {
-      return false;
+      return false; // si alguna no coincide;
     }
   }
   return true;
 }
 
-const string1 = "amor";
-const string2 = "roma";
+console.log(anagrama("amor", "roma"));
