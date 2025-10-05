b5108f5124e7753357833e08941af91192d273d2
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-10-05 14:43:04 -0300

    Corrijo nombre incorrecto del ejericio OrdenaLaLista.js

--- MasEjerciciosApartados/Ejercicios-Logica/Ejercicios-Basicos/OrdenaLaLista.js
+++ MasEjerciciosApartados/Ejercicios-Logica/Ejercicios-Basicos/OrdenaLaLista.js
@@ -11,15 +11,15 @@ function ordenarMatriz(matriz, orden = 'Asc') {
         }
     }
     
-    // Crear una copia para no modificar la matriz original
+
     const matrizOrdenada = [...matriz];
     
-    // Implementar el algoritmo Bubble Sort
+    
     const n = matrizOrdenada.length;
     
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - i - 1; j++) {
-            // Determinar si necesitamos intercambiar basado en el orden
+            
             let necesitaIntercambio = false;
             
             if (orden === 'Asc') {
@@ -28,7 +28,7 @@ function ordenarMatriz(matriz, orden = 'Asc') {
                 necesitaIntercambio = matrizOrdenada[j] < matrizOrdenada[j + 1];
             }
             
-            // Intercambiar elementos si es necesario
+            
             if (necesitaIntercambio) {
                 const temp = matrizOrdenada[j];
                 matrizOrdenada[j] = matrizOrdenada[j + 1];
@@ -40,12 +40,10 @@ function ordenarMatriz(matriz, orden = 'Asc') {
     return matrizOrdenada;
 }
 
-// Ejemplos de uso:
 console.log(ordenarMatriz([2, 4, 6, 8, 9], 'Asc'));  // [2, 4, 6, 8, 9]
 console.log(ordenarMatriz([2, 4, 6, 8, 9], 'Desc')); // [9, 8, 6, 4, 2]
 console.log(ordenarMatriz([5, 2, 8, 1, 9], 'Asc'));  // [1, 2, 5, 8, 9]
 console.log(ordenarMatriz([5, 2, 8, 1, 9], 'Desc')); // [9, 8, 5, 2, 1]
 console.log(ordenarMatriz([3, 1, 4, 1, 5], 'Asc'));  // [1, 1, 3, 4, 5]
 
-// Con orden por defecto (Asc)
 console.log(ordenarMatriz([3, 1, 4, 1, 5])); // [1, 1, 3, 4, 5]
