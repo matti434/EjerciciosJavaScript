950a2f5881c3f67230aa85f5885cd84113894ead
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-08 02:51:58 -0300

    feat: add avion and Pasajero classes in ejercicio10.js

--- javaScript/TP4-POO/Ejercicio10.js
+++ javaScript/TP4-POO/Ejercicio10.js
@@ -11,31 +11,53 @@ Agregar los 3 aviones al aeropuerto, buscar un avión y usar el método abordar.
 // cuantos hay, cual es la capacidad maxima,tiempo de ingreso
 */
 
-
-//crear una clase aeropuerto con las propiedades nombreAeropuerto y lista de aviones, esta clase deberá contener el método agregarAvion,
-class aeropuerto{
-
-  constructor(nombreAeropuerto,listaDeAviones){
-     this.nombreAeropuerto=nombreAeropuerto;
-     this.listaDeAviones=[];
+/*crear una clase aeropuerto con las propiedades nombreAeropuerto y lista de aviones, esta clase deberá contener el método agregarAvion
+ el cual recibirá un objeto de tipo Avión, además de un método buscarAvion el cual recibirá el nombre de un avión y devolverá información en caso de encontrarlo, 
+ si no lo encontró indicar con un mensaje.*/
+class aeropuerto {
+  constructor(nombreAeropuerto, listaDeAviones) {
+    this.nombreAeropuerto = nombreAeropuerto;
+    this.listaDeAviones = [];
   }
 
-
-  agregarAvion(avion){
+  agregarAvion(avion) {
     this.listaDeAviones.push(avion);
   }
-
 }
 
-class avion{
-   
-  constructor(nombre,capacidad,destino,listaDePasajeros){
-    this.nombre=nombre;
-    this.capacidad=capacidad;
-    this.destino=destino;
-    this.listaDePasajeros=[];
+/*Por su parte los aviones tendrán las propiedades: nombre, capacidad, destino, lista de pasajeros. 
+Los aviones tienen el método abordar el cual permite que un pasajero suba al avión solo si hay capacidad disponible en el mismo, 
+caso contrario mostrar un mensaje que indique que el avión está lleno.*/
+
+class avion {
+  constructor(nombre, capacidad, destino, listaDePasajeros) {
+    this.nombre = nombre;
+    this.capacidad = capacidad;
+    this.destino = destino;
+    this.listaDePasajeros = [];
   }
 
+  abordar(pasajero) {
 
+    if(!pasajero.pasaporte){
+      return ` El pasajero ${pasajero.nombre} no puede abordar: "Pasaporte invalido" `;
+    }
 
+    if (this.listaDePasajeros.length < this.capacidad) {
+      this.listaDePasajeros.push(pasajero);
+      return "Pasajero aborde exitosamente";
+    } else if (
+      this.listaDePasajeros.length > this.capacidad ||
+      this.listaDePasajeros.length === this.capacidad
+    ) {
+      return "El avion ya esta lleno";
+    }
+  }
+}
+
+class Pasajero {
+  constructor(nombre, pasaporte) {
+    this.nombre = nombre;
+    this.pasaporte = pasaporte;
+  }
 }
