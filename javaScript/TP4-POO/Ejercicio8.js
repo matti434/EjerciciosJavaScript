576dc7ec26d3a7b61f7d3a68e9bf6cb1f75a8a2e
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-08 02:08:57 -0300

    feat: add  Persona class with saludar and despedirse methods in Ejercicio8.js

--- javaScript/TP4-POO/Ejercicio8.js
+++ javaScript/TP4-POO/Ejercicio8.js
@@ -1,6 +1,8 @@
 /*8- Crea una clase llamada "Persona" que tenga las propiedades "nombre", "edad" y "profesión", y los métodos "saludar" y "despedirse". 
 Luego, crea dos objetos de la clase "Persona" con diferentes valores para sus propiedades y llama a sus métodos "saludar" y "despedirse".*/
 
+
+
 class Persona {
   constructor(nombre, edad, profesion) {
     this.nombre = nombre;
