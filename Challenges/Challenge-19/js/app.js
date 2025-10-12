40937844db342bc39df5163b70471417fe10ea3c
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-12 20:00:29 -0300

    fix: correct methods names to plural in Gestion.js and app.js

--- Challenges/Challenge-19/js/app.js
+++ Challenges/Challenge-19/js/app.js
@@ -6,8 +6,9 @@ import { Gestion } from "./clases/Gestion.js";
 
 const adminIntance = new Admin();
 const almacen = new Gestion();
-const formLogin = document.getElementById("form-login");
 
+
+const formLogin = document.getElementById("form-login");
 formLogin.addEventListener('submit' ,(e) =>{
     e.preventDefault();
 
@@ -22,19 +23,19 @@ formLogin.addEventListener('submit' ,(e) =>{
     }
 })
 
-const categoriaIntance = new Categoria();
 
 const formCategoria= document.getElementById("form-categoria");
-
 formCategoria.addEventListener("submit" ,(e) =>{
     e.preventDefault();
-
+    
     const nombre = document.getElementById("categoria-nombre").value;
     const descripcion = document.getElementById("categoria-descripcion").value;
-
-    almacen.agregarCategoria(nombre,descripcion);
+    
+    const categoriaIntance = new Categoria(nombre,descripcion);
+    almacen.agregarCategorias(categoriaIntance);
     alert("Categoría agregada con éxito");
+
     formCategoria.reset();
-})
+});
 
 
