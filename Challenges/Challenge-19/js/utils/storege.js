43b8c95c7f32c4cd0153994ee3ed53abbaf24f65
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-14 08:17:06 -0300

    feat: create exports in storege.js

---
+++ Challenges/Challenge-19/js/utils/storege.js
@@ -0,0 +1,8 @@
+export const guardarEnStorage = (clave,datos) => {
+     localStorage.setItem(clave,JSON.stringify(datos))
+}
+
+export const cargarDesdeStorage = (clave) => {
+    const datos = localStorage.getItem(clave);
+    return datos ? JSON.parse(datos) : [];
+}
