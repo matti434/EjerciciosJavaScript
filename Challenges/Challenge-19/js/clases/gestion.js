326745a7ebdf7b46d0203d39bf07fcbfed7fe6da
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-14 08:20:57 -0300

    feat: export methods from storage.js in gestion.js and modify guardar..... and cargar.... functions

--- Challenges/Challenge-19/js/clases/gestion.js
+++ Challenges/Challenge-19/js/clases/gestion.js
@@ -1,64 +1,55 @@
-export class Gestion{
+import { guardarEnStorage, cargarDesdeStorage } from "../utils/storege";
+export class Gestion {
+  #usuarios = [];
+  #productos = [];
+  #categorias = [];
 
-   #usuarios=[];
-   #productos=[];
-   #categorias=[];
+  constructor() {
+    this.#cargarDatos();
+  }
 
-   constructor(){
-     this.#cargarDatos();
-   }
-
-   agregarUsuario(usuario){
+  agregarUsuario(usuario) {
     this.#usuarios.push(usuario);
     this.#guardarUsuarios();
-   }
+  }
 
-   obtenerUsuarios(){
-    return[...this.#usuarios];
-   }
+  obtenerUsuarios() {
+    return [...this.#usuarios];
+  }
 
-   agregarProductos(producto){
+  agregarProductos(producto) {
     this.#productos.push(producto);
     this.#guardarProductos();
-   }
+  }
 
-   obtenerProductos(){
-    return[...this.#productos];
-   }
+  obtenerProductos() {
+    return [...this.#productos];
+  }
 
-   agregarCategorias(categoria){
+  agregarCategorias(categoria) {
     this.#categorias.push(categoria);
     this.#guardarCategorias();
-   }
+  }
 
-   obtenerCategorias(){
-    return[...this.#categorias];
-   }
+  obtenerCategorias() {
+    return [...this.#categorias];
+  }
 
-    #cargarDatos() {
-        this.#usuarios = this.#cargarDesdeStorage('usuarios');
-        this.#productos = this.#cargarDesdeStorage('productos');
-        this.#categorias = this.#cargarDesdeStorage('categorias');
-    }
+  #cargarDatos() {
+    this.#usuarios = cargarDesdeStorage("usuarios");
+    this.#productos = cargarDesdeStorage("productos");
+    this.#categorias = cargarDesdeStorage("categorias");
+  }
 
-    #guardarUsuarios(){
-        this.#guardarEnStorage('usuarios', this.#usuarios);
-    }
+  #guardarUsuarios() {
+    guardarEnStorage("usuarios", this.#usuarios);
+  }
 
-    #guardarProductos(){
-        this.#guardarEnStorage('productos', this.#productos);
-    }
+  #guardarProductos() {
+    guardarEnStorage("productos", this.#productos);
+  }
 
-    #guardarCategorias(){
-        this.#guardarEnStorage('categorias', this.#categorias);
-    }
-
-    #cargarDesdeStorage(clave) {
-        const datos = localStorage.getItem(clave);
-        return datos ? JSON.parse(datos) : [];
-    }
-    
-    #guardarEnStorage(clave, datos) {
-        localStorage.setItem(clave, JSON.stringify(datos));
-    }
+  #guardarCategorias() {
+    guardarEnStorage("categorias", this.#categorias);
+  }
 }
