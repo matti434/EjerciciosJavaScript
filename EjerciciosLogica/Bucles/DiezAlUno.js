30be84513ed487337f698e5db4e3e1051b9a46e3
Matías <matiasnicolaslazarte@gmail.com>
2026-02-27 18:04:11 -0300

    feat(bucles): add Bucles folder in EjerciciosLogica with include Exercise one and two from loop logic

---
+++ EjerciciosLogica/Bucles/DiezAlUno.js
@@ -0,0 +1,13 @@
+/* Mostrar números del 10 al 1.*/
+
+for(let i=10;i>=0;i--){
+console.log("Mostrando el numero con for:"+i);
+}
+
+
+let j = 10
+
+while(j>=0){
+    console.log("Mostrando el numero con while:"+j);
+    j--;
+}
---
+++ EjerciciosLogica/Bucles/UnoAlDiez.js
@@ -0,0 +1,14 @@
+/* Mostrar números del 1 al 10. */
+
+
+for(let i=1;i<=10;i++){
+    console.log("Mostrando el numero con for:"+i); 
+}
+
+let j=1;
+
+
+while(j<=10){
+    console.log("Mostrando el numero con while:"+j);
+    j++;
+}
